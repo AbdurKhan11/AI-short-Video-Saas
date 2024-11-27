cf4f9c1124dd81404d9de910ec90b745d3b62a20
Abdurrehman <127671775+AbdurKhan11@users.noreply.github.com>
2024-11-27 20:59:53 +0500

    third

---
+++ configs/scheme.js
@@ -0,0 +1,11 @@
+import { serial } from "drizzle-orm/mysql-core";
+import { pgTable } from "drizzle-orm/pg-core";
+
+export const User = pgTable('users',{
+    id:serial('id').primaryKey(),
+    name: varchar('name').notNull(),
+    email:varchar('email').notNull(),
+    image:varchar('imageURL'),
+    
+
+})
