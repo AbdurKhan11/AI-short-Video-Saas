92378af8c759b8a55d8dfe9aad1e16a8624aafa5
Abdurrehman <127671775+AbdurKhan11@users.noreply.github.com>
2024-11-28 20:28:35 +0500

    7th

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs/dist/types/components.server";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,12 +20,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        >
+          <Provider>
+             {children}
+          </Provider>
       </body>
+
     </html>
+    </ClerkProvider>
   );
 }
---
+++ app/provider.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Provider ({chidlren}){
+    return(
+        <div>
+            {chidlren}
+        </div>
+    )
+}
+
+export default Provider
