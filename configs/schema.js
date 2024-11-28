de73e6e252dc3477158d38c25dc6135e07db9fee
Abdurrehman <127671775+AbdurKhan11@users.noreply.github.com>
2024-11-28 19:34:25 +0500

    fifth

--- configs/schema.js
+++ configs/schema.js
@@ -1,7 +1,6 @@
-import { boolean, serial } from "drizzle-orm/mysql-core";
-import { pgTable } from "drizzle-orm/pg-core";
+import { boolean, serial, varchar, pgTable } from "drizzle-orm/pg-core";
 
-export const User = pgTable('users',{
+export const Users = pgTable('users',{
     id:serial('id').primaryKey(),
     name: varchar('name').notNull(),
     email:varchar('email').notNull(),
